d4662097b2e87ce0d1f6520114beeef1a6848407
Victor Widell <victor@topmost.se>
2019-03-26 19:09:58 +0100

    Replaced console UI with web app.

---
+++ src/main.ts
@@ -0,0 +1,124 @@
+import {
+	moveIsLegal,
+	getLegalMoves,
+	getBestMove,
+	move,
+	Board,
+} from "./othello";
+
+function printBoard(
+	board: Board,
+	markedPosition: number,
+	player: number,
+	userClickResolver?: (move: number) => void,
+): void {
+	const xpos = markedPosition % 8;
+	const ypos = Math.floor(markedPosition / 8);
+
+	let pl1count = 0;
+	let pl2count = 0;
+	for (let i = 0; i < 64; i++)
+		if (board[i] == 1) {
+			pl1count++;
+		} else if (board[i] == -1) {
+			pl2count++;
+		}
+
+	const status = document.createElement("p");
+	status.innerText =
+		"X:" +
+		pl1count +
+		", O:" +
+		pl2count +
+		", player: " +
+		(player == 1 ? "X" : "O");
+
+	const tbody = document.createElement("table");
+	for (let y = 0; y < 8; y++) {
+		const tr = document.createElement("tr");
+		for (let x = 0; x < 8; x++) {
+			const button = document.createElement("button");
+			userClickResolver &&
+				button.addEventListener("click", () => userClickResolver(x + y * 8));
+
+			button.style.width = "2em";
+			button.style.height = "2em";
+			if (y == ypos && x == xpos) {
+				button.style.backgroundColor = "palevioletred";
+			}
+
+			if (board[x + y * 8] == 1) button.innerText = "X";
+			else if (board[x + y * 8] == -1) button.innerText = "O";
+			else button.innerHTML = "&nbsp;";
+
+			const td = document.createElement("td");
+			td.appendChild(button);
+			tr.appendChild(td);
+		}
+		tbody.appendChild(tr);
+	}
+
+	const root = document.getElementById("root")!;
+	for (const child of Array.from(root.childNodes)) {
+		root.removeChild(child);
+	}
+	const table = document.createElement("table");
+	table.appendChild(tbody);
+	root.appendChild(status);
+	root.appendChild(table);
+}
+
+async function main(): Promise<void> {
+	let player = 1;
+	let board: Board = [
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, -1, 1, 0, 0, 0],
+		...[0, 0, 0, 1, -1, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+	];
+
+	let userClickResolver!: (move: number) => void;
+	let userClick = new Promise<number>(resolve => (userClickResolver = resolve));
+	printBoard(board, -1, player, userClickResolver);
+
+	for (;;) {
+		const moveList = getLegalMoves(board, player);
+		if (moveList.length) {
+			if (player == 1) {
+				const userMove = await userClick;
+
+				if (moveIsLegal(userMove, board, player)) {
+					board = move(userMove, board, player);
+					printBoard(board, userMove, player);
+					// Let the printed board render.
+					await new Promise(resolve => setTimeout(resolve, 200));
+					player = -player;
+				} else {
+					userClick = new Promise(resolve => (userClickResolver = resolve));
+					printBoard(board, -1, player, userClickResolver);
+				}
+			} else {
+				// AI
+				const aiMove = getBestMove(board, player);
+				board = move(aiMove, board, player);
+				userClick = new Promise(resolve => (userClickResolver = resolve));
+				printBoard(board, aiMove, player, userClickResolver);
+				player = -player;
+			}
+		} else {
+			player = -player;
+			const moveList = getLegalMoves(board, player);
+			if (!moveList.length) {
+				printBoard(board, -1, player);
+				alert("game over");
+				break;
+			}
+		}
+	}
+}
+
+main();
--- src/othello.ts
+++ src/othello.ts
@@ -1,4 +1,4 @@
-type Board = ReadonlyArray<number>;
+export type Board = ReadonlyArray<number>;
 
 // list-format:
 //  every list is a number[64] where the first element tells how long the list is.
@@ -27,7 +27,7 @@ const heuristicScores = [
 // defining offsets for the 8 directions. upp-left, upp, upp-right, ..., down-right. the order doesn't really matter.
 const offSets = [-9, -8, -7, -1, 1, 7, 8, 9];
 
-function getLegalMoves(board: Board, player: number): number[] {
+export function getLegalMoves(board: Board, player: number): number[] {
 	// loop through all squares to find legal moves and add them to the list.
 	const legalMoves = [];
 	for (let i = 0; i <= 63; i++) {
@@ -39,7 +39,11 @@ function getLegalMoves(board: Board, player: number): number[] {
 	return legalMoves;
 }
 
-function moveIsLegal(position: number, board: Board, player: number): boolean {
+export function moveIsLegal(
+	position: number,
+	board: Board,
+	player: number,
+): boolean {
 	if (board[position]) return false; // we may only put pieces in empty squares.
 
 	for (let direction = 0; direction < 8; direction++) {
@@ -78,7 +82,7 @@ function stepIsLegal(position: number, offSet: number): boolean {
 	return true;
 }
 
-function getBestMove(board: Board, player: number): number {
+export function getBestMove(board: Board, player: number): number {
 	const legalMoves = getLegalMoves(board, player);
 
 	let score = -Infinity;
@@ -161,7 +165,7 @@ function heuristicScore(board: Board, player: number): number {
 }
 
 // make shure you mAY move before you call this function.
-function move(position: number, board: Board, player: number): Board {
+export function move(position: number, board: Board, player: number): Board {
 	const newBoard = [...board];
 	newBoard[position] = player;
 
@@ -192,159 +196,3 @@ function move(position: number, board: Board, player: number): Board {
 
 	return newBoard;
 }
-
-// text-mode interface. sucks but works.
-
-// function  printMoves( moves: number[]): void{
-//   console.log("%i legal moves: ", moves[0]);
-//   for(let i=1; i<=moves[0]; i++){
-//     console.log(i+", ", moves[i]);}
-//   console.log("\n");
-// }
-
-function printBoard(
-	board: Board,
-	markedPosition: number,
-	player: number,
-): void {
-	const xpos = markedPosition % 8;
-	const ypos = Math.floor(markedPosition / 8);
-
-	console.log("xpos", xpos);
-	console.log("ypos", ypos);
-
-	let pl1count = 0;
-	let pl2count = 0;
-
-	for (let i = 0; i < 64; i++)
-		if (board[i] == 1) {
-			pl1count++;
-		} else if (board[i] == -1) {
-			pl2count++;
-		}
-
-	console.log("\n  player: " + (player == 1 ? "X" : "O"));
-	console.log("   X:" + pl1count + " O:" + pl2count);
-	console.log("  +-+-+-+-+-+-+-+-+");
-	for (let y = 0; y < 8; y++) {
-		let row = "  |";
-		for (let x = 0; x < 8; x++) {
-			if (board[x + y * 8] == 1) row += "X|";
-			else if (board[x + y * 8] == -1) row += "O|";
-			else row += " |";
-		}
-		if (y == ypos) row += " <-";
-		console.log(row);
-		console.log("  +-+-+-+-+-+-+-+-+");
-	}
-
-	let row = "";
-	for (let x = 0; x <= xpos; x++) {
-		row += "  ";
-	}
-	row += " ^";
-	console.log(row);
-	row = "";
-	for (let x = 0; x <= xpos; x++) {
-		row += "  ";
-	}
-	row += " |";
-	console.log(row);
-}
-
-// let the user pick a move. returns -1 if he/she wants to quit.
-async function getUserMove(
-	board: Board,
-	player: number,
-	markedPosition: number,
-): Promise<number> {
-	printBoard(board, markedPosition, player);
-
-	var stdin = process.stdin;
-	if (!stdin.setRawMode) {
-		throw new Error("Needs tty/raw mode.");
-	}
-	stdin.setRawMode(true);
-	stdin.resume();
-	stdin.setEncoding("utf8");
-
-	return new Promise(resolve => {
-		stdin.on("data", function(buffer) {
-			const key = buffer.toString();
-
-			// ctrl-c ( end of text )
-			if (key === "\u0003") {
-				process.exit();
-			}
-
-			if (key == "a" && stepIsLegal(markedPosition, -1)) {
-				markedPosition -= 1;
-			} else if (key == "d" && stepIsLegal(markedPosition, +1)) {
-				markedPosition += 1;
-			} else if (key == "w" && stepIsLegal(markedPosition, -8)) {
-				markedPosition -= 8;
-			} else if (key == "s" && stepIsLegal(markedPosition, +8)) {
-				markedPosition += 8;
-			}
-
-			printBoard(board, markedPosition, player);
-
-			if (key === " ") {
-				resolve(markedPosition);
-			}
-		});
-	});
-}
-
-async function main(): Promise<void> {
-	let player = 1;
-	let markedPosition = 0;
-	let board: Board = [
-		...[0, 0, 0, 0, 0, 0, 0, 0],
-		...[0, 0, 0, 0, 0, 0, 0, 0],
-		...[0, 0, 0, 0, 0, 0, 0, 0],
-		...[0, 0, 0, -1, 1, 0, 0, 0],
-		...[0, 0, 0, 1, -1, 0, 0, 0],
-		...[0, 0, 0, 0, 0, 0, 0, 0],
-		...[0, 0, 0, 0, 0, 0, 0, 0],
-		...[0, 0, 0, 0, 0, 0, 0, 0],
-	];
-
-	const moveList = getLegalMoves(board, player);
-	markedPosition = moveList[0];
-
-	for (;;) {
-		const moveList = getLegalMoves(board, player);
-		if (moveList.length) {
-			if (player == 1) {
-				// user input.
-				//        markedPosition = getBestMove(board, player);
-				//        markedPosition = moveList[1];
-				const userMove = await getUserMove(board, player, markedPosition);
-
-				console.log("moveIsLegal", moveIsLegal(userMove, board, player));
-
-				if (moveIsLegal(userMove, board, player)) {
-					board = move(userMove, board, player);
-					printBoard(board, userMove, player);
-					player = -player;
-				}
-			} else {
-				// AI
-				markedPosition = getBestMove(board, player);
-				board = move(markedPosition, board, player);
-				player = -player;
-			}
-		} else {
-			player = -player;
-			const moveList = getLegalMoves(board, player);
-			if (!moveList.length) {
-				printBoard(board, markedPosition, player);
-				console.log("  game over\n\n");
-				break;
-			}
-		}
-	}
-}
-
-main();
