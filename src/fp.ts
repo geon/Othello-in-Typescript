0a6bba9a0ca6e11edbb149ab584000b5079a7e58
Victor Widell <victor.widell@divid.se>
2026-06-21 15:18:51 +0200

    Refactor. Fixed typing.

--- src/fp.ts
+++ src/fp.ts
@@ -1,6 +1,7 @@
 export function randomArrayElement<T>(array: ReadonlyArray<T>): T {
-	if (!array.length) {
+	const element = array[Math.floor(Math.random() * array.length)];
+	if (!element) {
 		throw new Error("Can't pick an element from an empty array.");
 	}
-	return array[Math.floor(Math.random() * array.length)];
+	return element;
 }
