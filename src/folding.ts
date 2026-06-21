8527dd456937112483ce241c8025df0c40ead9ec
Victor Widell <victor.widell@divid.se>
2026-06-21 16:19:55 +0200

    Copy input.

--- src/folding.ts
+++ src/folding.ts
@@ -1,5 +1,21 @@
-import { Board } from "./othello";
+import { Board, Cell, coordToIndex, indexToCoord } from "./othello";
 
 export function foldVertically(board: Board): Board {
-	return board;
+	const folded: Cell[] = [
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+		...[0, 0, 0, 0, 0, 0, 0, 0],
+	] as const;
+
+	for (let index = 0; index < 64; ++index) {
+		const coord = indexToCoord(index);
+		folded[index] = board[coordToIndex(coord)]!;
+	}
+
+	return folded;
 }
